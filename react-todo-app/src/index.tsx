e49cc4ce9a1d51277b4b79c689a8bb65c39d0804
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-06-09 16:42:44 +0600

    added mdb and redux

--- react-todo-app/src/index.tsx
+++ react-todo-app/src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import App from "./App/App";
+import reducers from "./reducers";
+
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware()));
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +18,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+ 	<Provider store={store}>
+			<App />
+		</Provider>
   </React.StrictMode>
 );
 
---
+++ react-todo-app/src/types/actionsType.tsx
@@ -0,0 +1,36 @@
+import { todo } from "./storeType";
+
+// For Action Creator
+export interface markCompleteAction {
+	type: "MARK_COMPLETE";
+	id: string;
+}
+
+export interface markIncompleteAction {
+	type: "MARK_INCOMPLETE";
+	id: string;
+}
+
+export interface deleteTodoAction {
+	type: "DELETE_TODO";
+	id: string;
+}
+
+export interface createTodoAction {
+	type: "CREATE_TODO";
+	title: string;
+}
+
+export interface getTodos {
+	type: "GET_TODOS";
+}
+
+// For Reducers
+
+export const actionIds = {
+	MARK_COMPLETE: "MARK_COMPLETE",
+	MARK_INCOMPLETE: "MARK_INCOMPLETE",
+	DELETE_TODO: "DELETE_TODO",
+	CREATE_TODO: "CREATE_TODO",
+	GET_TODOS: "GET_TODOS",
+};
